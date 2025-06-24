8aba3499a55dfd48710f046fabcb58434b934767
786saqib.hussain <786saqib.hussain@gmail.com>
2025-06-23 22:56:11 -0700

    added a post endpoint

--- src/Services/Catalog/CatalogApi/Program.cs
+++ src/Services/Catalog/CatalogApi/Program.cs
@@ -3,8 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 //http request pipeline configuration
+
+app.MapCarter();
 
 app.Run();
